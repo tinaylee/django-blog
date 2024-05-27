ada76b7e9ac18d6e5311b736ba3eee57ce021fc8
Tina Lee <tina_lee@apple.com>
2024-05-27 14:32:34 -0700

    Created template for blogging list view

--- mysite/urls.py
+++ mysite/urls.py
@@ -17,8 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('blogging.urls')),
     path('', include('polling.urls')),
     path('admin/', admin.site.urls)
-
-
 ]
