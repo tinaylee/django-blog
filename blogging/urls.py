133c5d88d681105d43f563b9d7b6b469a1e27777
Tina Lee <tina_lee@apple.com>
2024-05-27 15:12:21 -0700

    Created detail view

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
+from blogging.views import detail_view
 from blogging.views import list_view
 
 urlpatterns = [
     path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', list_view, name="blog_detail")
+    path('posts/<int:post_id>/', detail_view, name="blog_detail")
 ]
