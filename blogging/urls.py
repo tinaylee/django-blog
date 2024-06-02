1567fb20264650a67b93837a0e6f6acb6886999c
Tina Lee <tina_lee@apple.com>
2024-06-01 22:35:20 -0700

    Added logic to exclude unpublished posts

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from blogging.views import detail_view
-from blogging.views import list_view
+from blogging.views import PostDetailView
+from blogging.views import PostListView
 
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail")
+    path('', PostListView.as_view(), name="blog_index"),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name="blog_detail")
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -2,24 +2,32 @@ from django.http import HttpResponse, HttpResponseRedirect, Http404
 from django.template import loader
 from blogging.models import Post
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type = "text/plain")
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'post_list'
+    queryset = Post.objects.exclude(published_date__exact=None)
+    template_name = 'blogging/list.html'
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+    queryset = Post.objects.exclude(published_date__exact=None)
+
+    def post(self, request, pk):
+        try:
+            post = self.get_object(queryset=queryset)
+        except Post.DoesNotExist:
+            raise Http404
+        context = {'object': post}
+        return render(request, 'blogging/detail.html', context)
+
 
 def detail_view(request, post_id):
+
     published = Post.objects.exclude(published_date__exact=None)
     try:
         post = published.get(pk=post_id)
