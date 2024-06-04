d4e204b8da458e82745c1b35d7611adc2c33d632
Tina Lee <tina_lee@apple.com>
2024-06-03 20:19:25 -0700

    Removed model from PostListView and PostDetailView

--- blogging/views.py
+++ blogging/views.py
@@ -7,13 +7,11 @@ from django.views.generic.detail import DetailView
 
 
 class PostListView(ListView):
-    model = Post
     context_object_name = 'post_list'
     queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
     template_name = 'blogging/list.html'
 
 class PostDetailView(DetailView):
-    model = Post
     template_name = 'blogging/detail.html'
     queryset = Post.objects.exclude(published_date__exact=None)
 
