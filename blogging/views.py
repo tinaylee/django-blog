c32a926cf69cfca0a7bfbcc80e942effaeebf9aa
Tina Lee <tina_lee@apple.com>
2024-06-03 20:14:01 -0700

    Set up PostListView to display in reverse chronological order

--- blogging/views.py
+++ blogging/views.py
@@ -9,7 +9,7 @@ from django.views.generic.detail import DetailView
 class PostListView(ListView):
     model = Post
     context_object_name = 'post_list'
-    queryset = Post.objects.exclude(published_date__exact=None)
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
     template_name = 'blogging/list.html'
 
 class PostDetailView(DetailView):
