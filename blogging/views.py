078c49e6b3ef0127957972cbc725de68c400af43
Tina Lee <e-mail address>
2024-06-07 19:56:19 -0500

    Removed unused imports

--- blogging/views.py
+++ blogging/views.py
@@ -1,5 +1,4 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
+from django.http import Http404
 from blogging.models import Post
 from django.shortcuts import render
 from django.views.generic.list import ListView
@@ -22,14 +21,3 @@ class PostDetailView(DetailView):
             raise Http404
         context = {'object': post}
         return render(request, 'blogging/detail.html', context)
-
-
-def detail_view(request, post_id):
-
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
