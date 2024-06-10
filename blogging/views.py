14f784041e1233fcb6ef3f30487cc19c1ef00799
Tina Lee <e-mail address>
2024-06-09 20:26:13 -0500

    Set up API, Fixed module not found errors, runserver working again

--- blogging/views.py
+++ blogging/views.py
@@ -3,6 +3,11 @@ from blogging.models import Post
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.contrib.auth.models import User
+from rest_framework import permissions, viewsets
+
+from blogging.models import Category
+from blogging.serializers import UserSerializer, PostSerializer, CategorySerializer
 
 
 class PostListView(ListView):
@@ -24,3 +29,19 @@ class PostDetailView(DetailView):
             raise Http404
         context = {"object": post}
         return render(request, "blogging/detail.html", context)
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-published_date')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('id')
